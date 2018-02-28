2a24a12b799eda722d684e12be8019ba009172f8
vitor-moymer <vitor@moymer.com>
2018-02-28 11:05:34 -0300

    Update erls_resource.erl

--- src/erls_resource.erl
+++ src/erls_resource.erl
@@ -61,7 +61,6 @@ request(State, Method, Path, Headers, Params, Body, Options) ->
                   Props -> <<"?", (encode_query(Props))/binary>>
               end)/binary>>,
     {Headers2, Options1, Body} = make_body(Body, Headers, Options),
-    io:format("Header ~p ~n",[Headers2]),
     Headers3 = default_header(<<"Content-Type">>, <<"application/json">>, Headers2),
     do_request(State, Method, Path1, Headers3, Body, Options1).
 
